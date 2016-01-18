ad8d34111b57744991ceb46fa2b4d5760cb1054e
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-01-18 17:11:27 +0600

    v8i_section tests passed

--- lib/ass_launcher/support/v8i_section.rb
+++ lib/ass_launcher/support/v8i_section.rb
@@ -1,7 +1,70 @@
 module AssLauncher
   module Support
+    # Implemet section of v8i file
     class V8iSection
+      attr_accessor :caption
+      attr_reader :fields
+      def initialize(caption, fields)
+        fail ArgumentError unless\
+          self.class.fields_required & fields.keys == self.class.fields_required
+        @caption = caption
+        @fields = fields
+      end
 
+      # Define required fields of v8i
+      # @return [Array<String>]
+      #  - Connect - connection string to infobase
+      def self.fields_required
+        %w( Connect )
+      end
+
+      # Define extra fields for v8i not defined 1C. This fields use
+      # admin tools for automate support infodases
+      # @return [Array<String>] of:
+      #  - AdmConnect - connection string to infobase for admin tools.
+      #    'Srvr' or 'File' connects only
+      #  - BaseCodeName - infobase configuration code name. For example
+      #    'Accounting', 'HRM', 'KzAccounting' etc.
+      #  - GetUpdateInfoURI - URI for configuration updateinfo file for example:
+      #    http://downloads.1c.ru/ipp/ITSREPV/V8Update/Configs/Accounting/20/82/
+      #  - BaseCurentVersion - curent configuration version
+      #  - GlobalWS - connection string for access to infobase from internet
+      #  - Vendor - configuration vendor. '1C' 'Rarys' etc.
+      def self.fields_extras
+        %w( AdmConnect
+            BaseCodeName
+            GetUpdateInfoURI
+            BaseCurentVersion
+            GlobalWS
+            Vendor )
+      end
+
+      # Define optional fields of v8i
+      # @return [Array<String>]
+      def self.fields_optional
+        'TODO'
+      end
+
+      def [](key)
+        fields[key]
+      end
+
+      def []=(key, value)
+        fields[key] = value
+      end
+
+      def key?(key)
+        fields.key?(key)
+      end
+
+      def to_s
+        res = ''
+        res << "[#{caption}]\r\n"
+        fields.each do |key, value|
+          res << "#{key}=#{value}\r\n"
+        end
+        res
+      end
     end
   end
 end
--- test/ass_launcher/support/v8i_section_test.rb
+++ test/ass_launcher/support/v8i_section_test.rb
@@ -8,18 +8,19 @@ class V8iSectionTest < Minitest::Test
   end
 
   def test_new_good
-    section = cls.new('caption',Connect:'conn_str')
-    assert_equal {Caption:'caption', Connect:'conn_str'}, section.fields
+    section = cls.new('caption', 'Connect' => 'conn_str')
+    assert_equal section.caption, 'caption'
+    assert_equal section.fields, {'Connect' => 'conn_str'}, section.fields
   end
 
   def test_new_whitout_fields_required
-    assert_raises ArgumenError do
+    assert_raises ArgumentError do
       cls.new('caption',{f1:'bla bla'})
     end
   end
 
   def tets_fields_required
-    assert_equal [:Connect], cls.fields_required
+    assert_equal ['Connect'], cls.fields_required
   end
 
   def test_fields_optional
@@ -27,25 +28,40 @@ class V8iSectionTest < Minitest::Test
   end
 
   def test_fields_extras
-    assert_equal [:AdmConnect,
-                  :BaseCodeName
-                  :GetUpdateInfoURI
-                  :BaseCurentVersion
-                  :GlobalWS
-                  :Vendor
-                  :ConfigName
-                  ], cls.fields_required
+    assert_equal ['AdmConnect',
+                  'BaseCodeName',
+                  'GetUpdateInfoURI',
+                  'BaseCurentVersion',
+                  'GlobalWS',
+                  'Vendor'
+    ], cls.fields_extras
   end
 
   def test_brackets
-    skip
+    section = cls.new('caption', {'Connect' => 'connect_str'})
+    assert_equal 'connect_str', section['Connect']
   end
 
-  def test_fields
-    skip
+  def test_brackets=
+    section = cls.new('caption', {'Connect' => 'connect_str'})
+    section['Connect'] = 'new_connect_str'
+    section['Field'] = 'field value'
+    assert_equal({'Connect'=>'new_connect_str', 'Field'=>'field value'},
+      section.fields)
   end
 
   def test_key?
-    skip
+    section = cls.new('caption', {'Connect' => 'connect_str'})
+    assert section.key? 'Connect'
+    assert ! section.key?('Field')
+  end
+
+  def test_to_s
+    expected = ''
+    expected << '[caption 1]'+"\r\n"
+    expected << 'Connect=connect_str'+"\r\n"
+    expected << 'Field1=Field 1 value'+"\r\n"
+    section = cls.new('caption 1', {'Connect'=>'connect_str','Field1'=>'Field 1 value'})
+    assert_equal expected, section.to_s
   end
 end
