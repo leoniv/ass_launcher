39d1d32eed6469582fa2a099afbdc7351d4861dc
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-01-27 14:27:49 +0600

    connection string tests passed

--- lib/ass_launcher/support/connection_string.rb
+++ lib/ass_launcher/support/connection_string.rb
@@ -1,27 +1,24 @@
 module AssLauncher
   module Support
     # Implement 1C connection string
+    # Mixin for connection string classes
     module ConnectionString
       class Error < StandardError; end
+      class ParseError < StandardError; end
       # Commonn connection string fields
       COMMON_FIELDS = %w(Usr Pwd LicDstr prmod Locale)
-      # Fields for server infobase
+      # Fields for server-infobase
       SERVER_FIELDS = %w(Srvr Ref)
-      # Fields for file infobase
+      # Fields for file-infobase
       FILE_FIELDS = %w(File)
       # Fields for infobase published on http server
       HTTP_FIELDS = %w(Ws)
-      # Field for make server infobase
-      IB_MAKER_FIELDS = %w(DBMS
-                           DBSrvr
-                           DB
-                           DBUID
-                           DBPwd
-                           SQLYOffs
-                           CrSQLDB
-                           SchJobDn
-                           SUsr
-                           SPwd)
+      # Proxy fields for accsess to infobase published on http server via proxy
+      PROXY_FIELDS = %w(Wsn Wsp WspAuto WspSrv WspPort WspUser WspPwd)
+      # Fields for makes server-infobase
+      IB_MAKER_FIELDS = %w(DBMS DBSrvr DB
+                           DBUID DBPwd SQLYOffs
+                           CrSQLDB SchJobDn SUsr SPwd)
       # Values for DBMS field
       DBMS_VALUES = %w(MSSQLServer PostgreSQL IBMDB2 OracleDatabase)
 
@@ -30,22 +27,41 @@ module AssLauncher
       # @return [Server | File | Http] instanse
       def self.[](connstr)
         case connstr
-        when /^File=/i then File.new(prase(connstr))
-        when /^Server=/i then Server.new(prase(connstr))
-        when /^Ws=/i then Http.new(parse(connstr))
+        when /(\W|\A)File\s*=\s*"/i then File.new(parse(connstr))
+        when /(\W|\A)Srvr\s*=\s*"/i then Server.new(parse(connstr))
+        when /(\W|\A)Ws\s*=\s*"/i then Http.new(parse(connstr))
         else
-          fail "Uncknown connstr `#{connstr}'"
+          fail ParseError, "Uncknown connstr `#{connstr}'"
         end
       end
 
-      # Parse connect string into hash
+      # Parse connect string into hash.
+      # Connect string have format:
+      #  'Field1="Value";Field2="Value";'
+      # Quotes ' " ' in value of field escape as doble quote ' "" '.
+      # Fields name convert to downcase [Symbol]
+      # @example
+      #  parse 'Field="""Value"""' -> {field: '"Value"'}
       # @param connstr [String]
+      # @return [Hash]
       def self.parse(connstr)
-        raise 'FIXME'
+        res = {}
+        connstr.split(';').each do |str|
+          str.strip!
+          res.merge!(parse_key_value str) unless str.empty?
+        end
+        res
       end
 
+      def self.parse_key_value(str)
+        fail ParseError, "Invalid string #{str}" unless\
+          /\A\s*(?<field>\w+)\s*=\s*"(?<value>.*)"\s*\z/i =~ str
+        { field.downcase.to_sym => value.gsub('""', '"') }
+      end
+      private_class_method :parse_key_value
+
       def is
-        name.split('::').last.downcase.to_sym
+        self.class.name.split('::').last.downcase.to_sym
       end
 
       def is?(symbol)
@@ -57,13 +73,14 @@ module AssLauncher
         fields.each do |f|
           result[f.downcase.to_sym] = get_property(f)
         end
+        result
       end
 
       def to_s(only_fields = nil)
         only_fields ||= fields
         result = ''
         only_fields.each do |f|
-          result << prop_to_s(f) if get_property(f)
+          result << "#{prop_to_s(f)};" unless get_property(f).to_s.empty?
         end
         result
       end
@@ -99,37 +116,52 @@ module AssLauncher
       end
 
       def prop_to_s(prop)
-        "#{prop}=\"#{get_property.gsub('"', '""')}\""
+        "#{fields_to_hash[prop.downcase.to_sym]}="\
+          +"\"#{get_property(prop).gsub('"', '""')}\""
       end
 
-      def required_fields_passed?(passed_fields)
+      def fields_to_hash
+        res = {}
+        fields.each do |f|
+          res[f.downcase.to_sym] = f
+        end
+        res
+      end
+
+      def required_fields_received?(received_fields)
         (required_fields.map { |f| f.downcase.to_sym }\
-          & passed_fields.keys.map { |k| k.downcase.to_sym }) == \
+          & received_fields.keys.map { |k| k.downcase.to_sym }) == \
           required_fields.map { |f| f.downcase.to_sym }
       end
 
-      # Connection string for server infobases
+      # Connection string for server-infobases
       class Server
         # Simple class host:port
         class ServerDescr
           attr_reader :host, :port
 
+          # @param host [String] hostname
+          # @param port [String] port number
           def initialize(host, port = nil)
-            @host = host
-            @port = port
+            @host = host.strip
+            @port = port.to_s.strip
           end
 
+          # Parse sting <srv_string>
+          # @param srv_str [String] string like 'host:port,host:port'
+          # @return [Arry<ServerDescr>]
           def self.parse(srv_str)
             r = []
             srv_str.split(',').each do |srv|
-              srv.strip
-              r << new(srv.chomp.split(':')) unless srv.empty?
+              srv.strip!
+              r << new(* srv.chomp.split(':')) unless srv.empty?
             end
             r
           end
 
+          # @return [String] formated 'host:port'
           def to_s
-            "#{host}:#{port}"
+            "#{host}" + (port.empty? ? '' : ":#{port}")
           end
         end
 
@@ -144,9 +176,8 @@ module AssLauncher
         include ConnectionString
 
         def initialize(hash)
-          fail ConnectionString::Error unless required_fields_passed?(hash)
+          fail ConnectionString::Error unless required_fields_received?(hash)
           _set_properties(hash)
-          @servers = ServerDescr.parce(@srvr)
         end
 
         # @return [Array<ServerDescr>]
@@ -155,7 +186,7 @@ module AssLauncher
         end
 
         def srvr=(str)
-          @srvers = ServerDescr.parce(str)
+          @servers = ServerDescr.parse(str)
           @srvr = str
         end
 
@@ -175,7 +206,7 @@ module AssLauncher
         end
       end
 
-      # Connection string for file infobases
+      # Connection string for file-infobases
       class File
         def self.required_fields
           FILE_FIELDS
@@ -188,21 +219,34 @@ module AssLauncher
         include ConnectionString
 
         def initialize(hash)
-          fail ConnectionString::Error unless required_fields_passed?(hash)
+          fail ConnectionString::Error unless required_fields_received?(hash)
           _set_properties(hash)
         end
       end
 
       # Connection string for infobases published on http server
-      class Http < File
+      class Http
         def self.required_fields
           HTTP_FIELDS
         end
 
+        def self.fields
+          required_fields | COMMON_FIELDS | PROXY_FIELDS
+        end
+
         include ConnectionString
 
+        def initialize(hash)
+          fail ConnectionString::Error unless required_fields_received?(hash)
+          _set_properties(hash)
+        end
+
         def uri
-          URI(ws)
+          require 'uri'
+          uri = URI(ws)
+          uri.user = wsn
+          uri.password = wsp
+          uri
         end
       end
     end
--- test/ass_launcher/support/connection_string_test.rb
+++ test/ass_launcher/support/connection_string_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 class TestServerDescr < Minitest::Test
 
   def cls
-    AssLauncher::Common::ConnectionString::ServerDesc
+    AssLauncher::Support::ConnectionString::Server::ServerDescr
   end
 
   def test_parse
     actual = cls.parse('server1:port1, server2:port2, server3, ')
     assert_equal 3, actual.size
     actual.each do |sd|
-      assert_instace_of cls, sd
+      assert_instance_of cls, sd
     end
     assert_equal 'server1', actual[0].host
     assert_equal 'port1', actual[0].port
@@ -25,61 +25,243 @@ end
 
 class TestConnectionString < Minitest::Test
 
-  class Server
+  class FakeConnStr
     def self.fields
       []
     end
 
     def self.required_fields
-      []
+      %w'F1 F2 F3'
     end
     include AssLauncher::Support::ConnectionString
   end
-
 
   def mod
     AssLauncher::Support::ConnectionString
   end
 
-  def test_fail
-    raise "FIXME"
+  def test_required_fields_received?
+    assert FakeConnStr.new.send(:required_fields_received?, { f1:'', f2:'', f3:'', f4:'' })
+    assert ! FakeConnStr.new.send(:required_fields_received?, { f2:'', f3:'', f4:'' })
   end
 
   def test_set_property
-    skip
+    mock = FakeConnStr.new
+    mock.expects(:fakeprop=).with('fake prop value')
+    mock.send(:set_property, 'FakeProp', 'fake prop value')
+  end
+
+  def test_get_property
+    mock = FakeConnStr.new
+    mock.expects(:fakeprop).returns('fake prop value')
+    assert_equal 'fake prop value', mock.send(:get_property, 'FakeProp')
   end
 
   def test_set_properties
-    skip
-  end
-
-  def test_constants
-    skip
+    mock = FakeConnStr.new
+    mock.expects(:set_property).with('FakeProp1', 'fake value 1')
+    mock.expects(:set_property).with('FakeProp2', 'fake value 2')
+    hash = {'FakeProp1'=>'fake value 1',
+            'FakeProp2'=>'fake value 2'}
+    mock.send(:_set_properties, hash)
   end
 
   def test_parse
-    skip
+    string = ' Field1="Value of f1"; Field2 ="Value of f2";Field3= "Value=""Quoted value""" '
+    hash = { field1: 'Value of f1',
+             field2: 'Value of f2',
+             field3: 'Value="Quoted value"'
+            }
+    assert_equal hash, mod.parse(string)
   end
 
-  def test_file_string
-    assert_instace_of AssLauncher::Common::ConnectionString::File, mod['File="C:\Bla\Bla"']
-    assert_instace_of AssLauncher::Common::ConnectionString::File, mod['File="C:/Bla/Bla"']
+  def test_fail_parse
+    assert_raises AssLauncher::Support::ConnectionString::ParseError do
+      string = '+Field=""'
+      mod.parse string
+    end
   end
 
-  def test_server_string
-    assert_instace_of AssLauncher::Common::ConnectionString::Server, mod['Srvr="host:port, host:port";Ref="IbName";']
+  def test_to_hash
+    mock = FakeConnStr.new
+    mock.expects(:get_property).with('FakeProp1').returns('fake value 1')
+    mock.expects(:get_property).with('FakeProp2').returns('fake value 2')
+    mock.expects(:fields).returns(%w'FakeProp1 FakeProp2')
+    hash = {:fakeprop1 => 'fake value 1',
+            :fakeprop2 => 'fake value 2'}
+    assert_equal hash, mock.to_hash
   end
 
-  def test_http_string
-    assert_instace_of AssLauncher::Common::ConnectionString::Http, mod['Ws="http://example.org";']
+  def test_fields
+    FakeConnStr.expects(:fields)
+    FakeConnStr.new.fields
+  end
+
+  def test_fields_to_hash
+    mock = FakeConnStr.new
+    mock.expects(:fields).returns(%w'F1 F2 F3')
+    assert_equal({f1:'F1', f2:'F2', f3:'F3'}, mock.send(:fields_to_hash))
+  end
+
+  def test_prop_to_s
+    mock = FakeConnStr.new
+    mock.expects(:fields_to_hash).returns({fakeprop:'FakeProp', emptyprop:'EmptyProp'}).at_least_once
+    mock.expects(:get_property).with('FakeProp').returns('Fake "value"')
+    mock.expects(:get_property).with('EmptyProp').returns('')
+    assert_equal 'FakeProp="Fake ""value"""', mock.send(:prop_to_s, 'FakeProp')
+    assert_equal 'EmptyProp=""', mock.send(:prop_to_s, 'EmptyProp')
+  end
+
+  def test_to_s
+    FakeConnStr.expects(:fields).returns(%w'F1 EmptyField F3').at_least_once
+    mock = FakeConnStr.new
+    mock.expects(:f1).returns('value 1').twice
+    mock.expects(:emptyfield).returns('').once
+    mock.expects(:f3).returns('value 3').twice
+    string = 'F1="value 1";F3="value 3";'
+    assert_equal string, mock.to_s
+  end
+
+  def test_to_s_with_param
+    FakeConnStr.expects(:fields).returns(%w'F1 EmptyField F3')
+    mock = FakeConnStr.new
+    mock.expects(:f1).never
+    mock.expects(:emptyfield).returns('').once
+    mock.expects(:f3).returns('value 3').twice
+    string = 'F3="value 3";'
+    assert_equal string, mock.to_s([:emptyfield, :f3])
+  end
+
+  def test_build_file_string
+    ['File="C:/Bla/Bla"',
+     'File="\\\\Bla\\Bla"',
+     'File="//Bla/Bla"'].each do |str|
+      AssLauncher::Support::ConnectionString.expects(:parse)
+      AssLauncher::Support::ConnectionString::File.expects(:new).returns('FakeFile')
+      assert_equal 'FakeFile', mod[str]
+    end
+  end
+
+  def test_build_server_string
+    AssLauncher::Support::ConnectionString.expects(:parse)
+    AssLauncher::Support::ConnectionString::Server.expects(:new).returns('FakeServer')
+    assert_equal 'FakeServer' , mod['Srvr="host:port, host:port";Ref="IbName";']
+  end
+
+  def test_build_http_string
+    AssLauncher::Support::ConnectionString.expects(:parse)
+    AssLauncher::Support::ConnectionString::Http.expects(:new).returns('FakeHttp')
+    assert_equal 'FakeHttp', mod['Ws="http://example.org";']
+  end
+
+  def test_fail_bild_string
+    assert_raises AssLauncher::Support::ConnectionString::ParseError do
+      mod['BadString="']
+    end
   end
 
   def test_is
-    assert_equal :server, Server.is
+    assert_equal :fakeconnstr, FakeConnStr.new.is
   end
 
   def test_is?
-    assert Server.is?, :server
+    assert FakeConnStr.new.is? :fakeconnstr
+  end
+end
+
+class TestConnectionStringServer < Minitest::Test
+  def cls
+    AssLauncher::Support::ConnectionString::Server
   end
 
+  def test_initialize
+    hash = {srvr:'Server1:Port1,Server2:port2', ref:'ibname'}
+    AssLauncher::Support::ConnectionString::Server::ServerDescr.expects(:parse).returns('fake parsed servers')
+    inst = cls.new(hash)
+    assert_equal 'fake parsed servers', inst.servers
+    assert_equal 'Server1:Port1,Server2:port2', inst.srvr_raw
+  end
+
+  def test_initialize_fail
+    hash = {f1:'', f2:''}
+    assert_raises AssLauncher::Support::ConnectionString::Error do
+      cls.new(hash)
+    end
+  end
+
+  def test_srvr
+    mock = Class.new(cls) do
+      def initialize
+      end
+    end.new
+    mock.expects(:servers).returns(%w'1 2 3')
+    assert_equal '1,2,3', mock.srvr
+  end
+
+  def test_dbms
+    mock = Class.new(cls) do
+      def initialize
+      end
+    end.new
+    mock.dbms = 'MSSQLServer'
+    assert_equal 'MSSQLServer', mock.dbms
+    mock.dbms = 'MSSQLServer'.upcase
+    assert_equal 'MSSQLServer'.upcase, mock.dbms, 'should be not case insensitive'
+  end
+
+  def test_dbms_fail
+    mock = Class.new(cls) do
+      def initialize
+      end
+    end.new
+    assert_raises ArgumentError do
+      mock.dbms = 'Bad dbms'
+    end
+  end
+end
+
+class TestConnectionStringFile < Minitest::Test
+  def cls
+    AssLauncher::Support::ConnectionString::File
+  end
+
+  def test_initialize
+    hash = {file: 'path'}
+    assert_equal 'path', cls.new(hash).file
+  end
+
+  def test_initialize_fail
+    hash = {}
+    assert_raises AssLauncher::Support::ConnectionString::Error do
+      cls.new hash
+    end
+  end
+end
+
+class TestConnectionStringHttp < Minitest::Test
+  def cls
+    AssLauncher::Support::ConnectionString::Http
+  end
+
+  def test_initialize
+    hash = {ws: 'http://example.com'}
+    assert_equal 'http://example.com', cls.new(hash).ws
+  end
+
+  def initialize_fail
+    hash = {}
+    assert_raises AssLauncher::Support::ConnectionString::Error do
+      cls.new hash
+    end
+  end
+
+  def test_uri
+    mock = Class.new(cls) do
+      def initialize
+
+      end
+    end.new
+    mock.expects(:ws).returns('http://exaple.com')
+    assert_kind_of ::URI, mock.uri
+  end
 end
