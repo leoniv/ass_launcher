06e5141c57eacb6ac0c0472c09789087a8dc54d1
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-05-10 10:35:25 +0600

    extract DslHelpers into dsl_helpers.rb

--- lib/ass_launcher/enterprise/cli/spec_dsl.rb
+++ lib/ass_launcher/enterprise/cli/spec_dsl.rb
@@ -3,64 +3,11 @@
 module AssLauncher
   module Enterprise
     module Cli
+      # DSL for describe 1C:Enterprise command-line interface
+      # @api public
       module SpecDsl
+        require 'ass_launcher/enterprise/cli/spec_dsl/dsl_helpers'
         require 'uri'
-        module DslHelpers
-          # Translating stub
-          def _t(s)
-            s
-          end
-
-          def defined_modes
-            AssLauncher::Enterprise::Cli::DEFINED_MODES
-          end
-          private :defined_modes
-
-          def described_modes
-            @described_modes ||= {}
-          end
-
-          def parameters_groups
-            @parameters_groups ||= {}
-          end
-
-          def parameters
-            @parameters ||= Parameters::ParametersList.new
-          end
-
-          attr_accessor :current_modes
-          private :current_modes, :current_modes=
-
-          attr_accessor :current_group
-          private :current_group, :current_group=
-
-          def parents_stack
-            @parents_stack ||= []
-          end
-          private :parents_stack
-
-          def current_parent
-            parents_stack[0]
-          end
-          private :current_parent
-
-          def new_param(klass, name, desc, binary_matcher, **options, &block)
-            p = klass.new(name, desc, binary_matcher, current_group,
-                          current_modes, current_parent, **options)
-            return unless p.match?(binary_wrapper, run_mode)
-            parameters << p
-            eval_sub_params(p, &block) if block_given?
-          end
-          private :new_param
-
-          def eval_sub_params(p, &block)
-            parents_stack.unshift p
-            instance_eval(&block)
-            parents_stack.shift
-          end
-          private :eval_sub_params
-        end
-
         include DslHelpers
 
         def enterprise_version(v = '0')
@@ -147,7 +94,7 @@ module AssLauncher
               URI(value)
             rescue
               raise ArgumentError,
-                "Invalid URL for parameter `#{n}': `#{value}'"
+                    "Invalid URL for parameter `#{n}': `#{value}'"
             end
             value
           end
@@ -165,7 +112,7 @@ module AssLauncher
               Float(value)
             rescue
               raise ArgumentError,
-                "Invalid Number for parameter `#{n}': `#{value}'"
+                    "Invalid Number for parameter `#{n}': `#{value}'"
             end
             value
           end
