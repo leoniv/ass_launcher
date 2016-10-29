c4f938ca571c1e08024822faadefd6e4ecbeb707
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-10-29 12:37:44 +0700

    add createinfobase and other features into dev-helper

--- bin/lib/dev_helper/designer.rb
+++ bin/lib/dev_helper/designer.rb
@@ -8,23 +8,10 @@ module DevHelper
           '1C designer wrapper'
         end
 
-        class Run < Clamp::Command
+        class Designer < Clamp::Command
           include AssLauncher::Api
           option ['-v', '--version'], 'VERSION', 'specify of platform version',
             required: true
-
-          def execute
-            conn_str = tmp_ib
-            cl.command(:designer) do
-              connection_string conn_str
-            end.run.wait
-          ensure
-            rm_ib
-          end
-
-          def rm_ib
-            FileUtils.rm_r tmp_ib_path if File.exists? tmp_ib_path
-          end
 
           def cl
             @cl ||= cl_get
@@ -35,15 +22,62 @@ module DevHelper
             signal_usage_error "Platform version `#{version}' not found" if r.nil?
             r
           end
+        end
+
+        module ConnSring
+          def self.included(base)
+            base.option ['-b', '--info-base'], 'PATH',
+              baner(base),
+              required: _required(base) do |s|
+              @conn_str = cs_file(file: s)
+            end
+            base.send :attr_reader, :conn_str
+          end
+
+          def self._required(base)
+            base.respond_to? :conn_str_required?
+          end
+
+          def self.baner(base)
+            r = 'specify infobase path.'
+            r << ' If not specify it will make tmp infobase' unless\
+              _required(base)
+            r
+          end
+        end
+
+        class Run < Designer
+          include ConnSring
+          option ['-a', '--args'], '["ARG", "VAL" ...]',
+            'specify designer cmd arguments like Ruby Array' do |s|
+            @args = eval(s)
+            fail ArgumentError,
+              'specify designer cmd arguments like Ruby Array' unless\
+              @args.is_a? Array
+          end
+
+          def execute
+            conn_str_ = conn_str || tmp_ib
+            args_ = args || []
+            cl.command(:designer, args_) do
+              connection_string conn_str_
+            end.run.wait.result.verify!
+          ensure
+            rm_tmp_ib
+          end
+
+          def rm_tmp_ib
+             FileUtils.rm_r tmp_ib_path if File.exists? tmp_ib_path
+          end
 
           def tmp_ib
-            rm_ib
-            conn_str = cs_file(:file => tmp_ib_path)
+            rm_tmp_ib
+            conn_str_ = cs_file(:file => tmp_ib_path)
             process_holder = cl.command(:createinfobase) do
-              connection_string conn_str
+              connection_string conn_str_
             end.run.wait
             process_holder.result.verify!
-            conn_str
+            conn_str_
           end
 
           def tmp_ib_path
@@ -55,6 +89,35 @@ module DevHelper
           end
         end
 
+        class CreateInfobase < Designer
+
+          def self.conn_str_required?
+            true
+          end
+
+          include ConnSring
+
+          option ['-t', '--template'], 'PATH[.cf|.dt]',
+            'Create infobase from template' do |s|
+            @template = s
+          end
+
+          def execute
+            $stdout.puts "Success make ib: `#{make_infobase}'"
+          end
+
+          def make_infobase
+            conn_str_ = conn_str
+            template_ = template
+            cl.command(:createinfobase) do
+              connection_string conn_str_
+              useTemplate template_ if template_
+            end.run.wait.result.verify!
+            conn_str_
+          end
+
+        end
+
         class Platforms < Clamp::Command
           include AssLauncher::Api
           def execute
@@ -63,6 +126,7 @@ module DevHelper
         end
 
         subcommand 'run', 'run of designer', Run
+        subcommand 'createinfobase', 'create file infrmation base', CreateInfobase
         subcommand 'platforms', 'show list of instaled platforms', Platforms
       end
     end
