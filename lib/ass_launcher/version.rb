a667da8ad6335e3533db443c439705b35e4180c2
Leonid Vlasov <leoniv.vlasov@gmail.com>
2017-01-31 11:32:16 +0700

    switch version to 0.2.1

--- lib/ass_launcher/version.rb
+++ lib/ass_launcher/version.rb
@@ -1,5 +1,5 @@
 module AssLauncher
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
   module KNOWN_ENTERPRISE_VERSIONS
     require 'ass_launcher/enterprise/cli_defs_loader'
     extend AssLauncher::Enterprise::CliDefsLoader
