17d768917db83cddc1ec7906f51f161b8b49d718
Leonid Vlasov <leoniv.vlasov@gmail.com>
2018-05-13 07:56:00 +0700

    v0.5.0 fix #19

--- test/ass_launcher_test.rb
+++ test/ass_launcher_test.rb
@@ -6,7 +6,7 @@ class AssLauncherTest < Minitest::Test
   end
 
   def test_known_enterprise_versions
-    expects = %w{8.2.17 8.2.18 8.3.3 8.3.4 8.3.5 8.3.6 8.3.7 8.3.8 8.3.9
+    expects = %w{8.1.0 8.2.17 8.2.18 8.3.3 8.3.4 8.3.5 8.3.6 8.3.7 8.3.8 8.3.9
                  8.3.10 8.3.11}
     assert_equal expects.join(', '),
       AssLauncher::KNOWN_ENTERPRISE_VERSIONS.get.join(', ')
