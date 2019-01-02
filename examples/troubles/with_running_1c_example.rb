c03ce2de437b87ab64693cd61726651eadc28365
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-01-02 10:42:29 +0700

    ch examples/troubles

--- examples/troubles/with_running_1c_example.rb
+++ examples/troubles/with_running_1c_example.rb
@@ -26,7 +26,7 @@ module Examples
         command.run.wait
       end
 
-      it 'Designer show dialg with error message for serevr infobase' do
+      it 'Designer show dialog with error message for serevr infobase' do
         skip unless TROUBLES_EXECUTE_CONTROL::SHOW_STUPID_GUI
         designer = CLIENTS::THICK.command :designer do
             _S 'example.org/fake_ib'
@@ -35,34 +35,6 @@ module Examples
 
         # It show GUI
         designer.run.wait
-      end
-
-      describe 'Solution. For file infobase only!!!' do
-        it 'Arguments builder check existing path for /F parameter' do
-          proc {
-            CLIENTS::THICK.command(:enterprise) do
-              _F 'bad infobase path'
-            end
-          }.must_raise ArgumentError
-        end
-
-        it 'Connection string fails in #to_args method if path not exists' do
-          extend AssLauncher::Api
-          conns = cs_file file: 'bad infobase path'
-
-          # Call #to_args directly
-          proc {
-            CLIENTS::THIN.command(conns.to_args)
-          }.must_raise Errno::ENOENT
-
-          # Passing connection string into arguments builder
-          # have the same effect
-          proc {
-            CLIENTS::THIN.command do
-              connection_string conns
-            end
-          }.must_raise Errno::ENOENT
-        end
       end
     end
 
@@ -87,20 +59,6 @@ module Examples
                    '/SetPredefinedDataUpdate','Bad value'
         ])
         command.run.wait.result.exitstatus.must_equal 0
-      end
-
-      it 'One of the rare cases when 1C:Enterprise verifies of paramter value' do
-        command = CLIENTS::THICK\
-          .command(:designer, ['/F', TMP::EMPTY_IB]) do
-            checkModules do
-              thinClient
-              eXtension 'bad value'
-            end
-          end
-        command.run.wait.result.exitstatus.wont_equal 0
-
-        # But doesn't explains cause of error
-        command.process_holder.result.assout.must_equal ''
       end
 
       describe 'Solutions this troubles with arguments builder' do
@@ -162,34 +120,6 @@ module Examples
 
         source_string.wont_equal ret_string
         ret_string.must_equal 'Hello \\\\\\'
-      end
-
-      describe 'Solutions' do
-        it 'Check value /C parameter with arguments builder' do
-          proc {
-            CLIENTS::THIN.command do
-              _C '"'
-            end
-          }.must_raise ArgumentError
-        end
-
-        it 'Passing value with single quote char' do
-          source_string = 'Hello \'World\''
-
-          enterprise = CLIENTS::THICK\
-            .command :enterprise do
-            _C source_string
-            connection_string TMP::EMPTY_IB_CS
-            _Debug
-            debuggerURL 'tcp://localhost'
-            _Execute TEMPLATES::HELLO_EPF
-          end
-
-          enterprise.run.wait
-          ret_string = enterprise.process_holder.result.assout.strip
-
-          source_string.must_equal ret_string
-        end
       end
     end
 
